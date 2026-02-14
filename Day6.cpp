3099e7613d50e8e16a34e099bd5c8779ab799875
Pranshu Garg <pranshunikhil2007@gmail.com>
2026-02-14 13:18:29 +0530

    Day-6

--- Day6.cpp
+++ Day6.cpp
@@ -1,5 +1,6 @@
 //Day 6 -> LinkedList cycle detection I & II 
 // Sort List using merge sort
+//ReorderList (ZigZag LinkedList)
 
 //problem link-> https://leetcode.com/problems/linked-list-cycle/description/
 //level-> medium
@@ -141,5 +142,68 @@
 //         ListNode* left=sortList(head);
 //         ListNode* right=sortList(rightHead);
 //         return merge(left,right);
+//     }
+// };
+
+//problem link->https://leetcode.com/problems/reorder-list/
+//level-> medium
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// class Solution {
+// //approach-> divide in mid and reverse the second half and then alternate merging
+// public:
+//     ListNode* splitAtMid(ListNode* head){
+//         ListNode* slow=head;
+//         ListNode* fast=head;
+//         ListNode* prev=NULL;
+//         while(fast!=NULL && fast->next!=NULL){
+//             prev=slow;
+//             slow=slow->next;
+//             fast=fast->next->next;
+//         }
+//         if(prev!=NULL){
+//             prev->next=NULL;
+//         }
+//         return slow;
+//     }
+//     ListNode* reverseLL(ListNode* head){
+//         ListNode* prev=NULL;
+//         ListNode* curr=head;
+//         ListNode* nextNode=NULL;
+//         while(curr!=NULL){
+//             nextNode=curr->next;
+//             curr->next=prev;
+//             prev=curr;
+//             curr=nextNode;
+//         }
+//         return prev;
+//     }
+//     void reorderList(ListNode* head) {
+//         ListNode* rightHead=splitAtMid(head);
+//         ListNode*rightRevHead=reverseLL(rightHead);
+//         //alternate merging
+//         ListNode* left=head;
+//         ListNode* right=rightRevHead;
+//         ListNode* tail=right;
+//         while(left!=NULL && right!=NULL){
+//             ListNode* nextLeft=left->next;
+//             ListNode* nextRight=right->next;
+//             left->next=right;
+//             right->next=nextLeft;
+//             tail=right;
+//             left=nextLeft;
+//             right=nextRight;
+//         }
+//         if(right!=NULL){
+//             tail->next=right;
+//         }
 //     }
 // };
