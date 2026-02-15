0a933787074d66b5414be1a726cf2c467243de6d
Pranshu Garg <pranshunikhil2007@gmail.com>
2026-02-15 12:32:06 +0530

    Day-7

--- Day7.cpp
+++ Day7.cpp
@@ -1,4 +1,5 @@
 //Day 7 ->Intersection of two linkedlist
+//swapping nodes 
 
 //problem link->https://leetcode.com/problems/intersection-of-two-linked-lists/description/
 //level->easy
@@ -56,5 +57,44 @@
 //             }
 //         }
 //         return NULL;
+//     }
+// };
+
+
+//problem link->https://leetcode.com/problems/swapping-nodes-in-a-linked-list/description/
+//level->medium
+//approach-> only swapping the values not the complete node
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+// class Solution {
+// public:
+//     ListNode* swapNodes(ListNode* head, int k) {
+//         int c=0;
+//         ListNode* temp=head;
+//         while(temp!=NULL){
+//             c++;
+//             temp=temp->next;
+//         }
+//         //size of linekdlist = c
+//         ListNode* tempA=head;
+//         ListNode* tempB=head;
+//         for(int i=0;i<k-1;i++){
+//             tempA=tempA->next;
+//         }
+//         for(int j=0;j<c-k;j++){
+//             tempB=tempB->next;
+//         }
+//         int tempo=tempA->val;
+//         tempA->val=tempB->val;
+//         tempB->val=tempo;
+//         return head;
 //     }
 // };
